cda7048004936f974fdb88c462833f0f31acdec8
= <lucas.burdell@blackburn.edu>
2017-10-25 23:44:19 -0500

    change formatting

--- question.handlebars.template.js
+++ question.handlebars.template.js
@@ -1,15 +1,15 @@
-var questionTemplate = (function() {
+(function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['question'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<div id=\"question"
+  return "<div class=\"questiondiv\" id=\"question"
     + alias4(((helper = (helper = helpers.question_number || (depth0 != null ? depth0.question_number : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"question_number","hash":{},"data":data}) : helper)))
     + "\">\r\n<h4><strong>"
     + alias4(((helper = (helper = helpers.question_title || (depth0 != null ? depth0.question_title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"question_title","hash":{},"data":data}) : helper)))
     + "</strong></h4>\r\n<p class=\"text\">"
     + alias4(((helper = (helper = helpers.question_text || (depth0 != null ? depth0.question_text : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"question_text","hash":{},"data":data}) : helper)))
-    + "</p>\r\n<div id=\"question"
+    + "</p>\r\n<div class=\"questiondiv2\" id=\"question"
     + alias4(((helper = (helper = helpers.question_number || (depth0 != null ? depth0.question_number : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"question_number","hash":{},"data":data}) : helper)))
     + "buttons\">\r\n<a class=\"btn btn-default\" id=\"question"
     + alias4(((helper = (helper = helpers.question_number || (depth0 != null ? depth0.question_number : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"question_number","hash":{},"data":data}) : helper)))
--- questions.js
+++ questions.js
@@ -3,6 +3,6 @@ var questions = {
 	"question_1": {"question_number" : 1, "question_title": "Question 1", "question_text": "Transference is valuable in therapy because it provides clients with the opportunity to reexperience a variety of feelings from early childhood."},
 	"question_2": {"question_number" : 2, "question_title": "Question 2", "question_text": "The social determinants of personality are far more powerful than psychosexual determinants."},
 	"question_3": {"question_number" : 3, "question_title": "Question 3", "question_text": "A therapist should challenge clients with the ways in which they are living an unauthentic life."},
-	"question_4": {"question_number" : 4, "question_title": "Question 4", "question_text": "I like to devise experiments designed to increase clients’ self-awareness of what they are doing and how they are doing it."},
+	"question_4": {"question_number" : 4, "question_title": "Question 4", "question_text": "I like to devise experiments designed to increase clients' self-awareness of what they are doing and how they are doing it."},
 	"question_5": {"question_number" : 5, "question_title": "Question 5", "question_text": "It is important to ask clients about their earliest recollections."}	
 }
